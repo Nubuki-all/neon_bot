83d4a82a05d0c27f8a09f5f61a6504c33715ec3f
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-02-07 01:38:17 +0100

    update

--- bot/utils/os_utils.py
+++ bot/utils/os_utils.py
@@ -61,7 +61,7 @@ def s_remove(*filenames, folders=False):
             try:
                 shutil.rmtree(_dir)
             except Exception:
-                pass
+                log(Exception)
         return
     for filename in filenames:
         try:
