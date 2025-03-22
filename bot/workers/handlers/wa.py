e870a71c9dff9e9cccc1d44909ad5f8793fc5172
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-03-22 22:21:03 +0100

    update wa.py

--- bot/workers/handlers/wa.py
+++ bot/workers/handlers/wa.py
@@ -255,9 +255,11 @@ async def send_deleted_msgs(event, chat_id, del_ids, verbose=False):
     msgs = await msg_store.get_messages_from_ids(chat_id, del_ids)
     if not msgs and verbose:
         sep = " ,"
-        await event.reply(
+        return await event.reply(
             f"*Could not find message(s) with ID(s):* '{list_to_str(del_ids, sep)}'"
         )
+    elif not msgs:
+        return await event.reply("*No recently deleted messages found.*")
     chain_reply = event
     for msg in msgs:
         await msg.reply(".")
