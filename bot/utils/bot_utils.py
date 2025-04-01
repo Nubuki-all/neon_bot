1caeb99495976233b92f6bf22461b1634b8187f2
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-04-01 14:59:57 +0100

    update bot_utils.py

--- bot/utils/bot_utils.py
+++ bot/utils/bot_utils.py
@@ -203,7 +203,7 @@ def waiting_for_turn():
 async def shutdown_services():
     await bot.client.disconnect()
     await bot.requests.close()
-    await bot.stop_back_up = True
+    bot.stop_back_up = True
     await bot.backup_wa_db()
     if bot.pending_saved_messages:
         if not bot.auto_save_msg_is_running:
