c4299bf9c508235f70c579f448c2f321e121ff26
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-05-15 00:59:54 +0000

    pyLint: auto-fixes

--- bot/utils/ytdl_utils.py
+++ bot/utils/ytdl_utils.py
@@ -364,8 +364,10 @@ class YoutubeDLHelper:
                         fmt["format_id"]
                         for fmt in result["formats"]
                         if (
-                            (fmt.get("vcodec") and
-                            fmt.get("vcodec", "").startswith("avc1"))
+                            (
+                                fmt.get("vcodec")
+                                and fmt.get("vcodec", "").startswith("avc1")
+                            )
                             or (
                                 fmt.get("vcodec") == "h264"
                             )  # Handle both representations
