371571e02915609f6f100c186a6ea7216f80e89e
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2026-06-22 11:04:31 +0100

    debug

--- bot/pkgs/insta_dl.py
+++ bot/pkgs/insta_dl.py
@@ -406,7 +406,12 @@ def _parse_gql_media(data: dict) -> List[DownloadResult]:
     items = []
 
     if typename in ("GraphVideo", "XDTGraphVideo"):
-        video_url = data["video_url"]
+        try:
+            video_url = data["video_url"]
+        except KeyError:
+            _log_.info("Missing 'video_url' in data. Keys:", data.keys())
+            _log_.info("Full data:", json.dumps(data, indent=2, default=str))
+            raise
         display_url = data.get("display_url", "")
         items.append(
             DownloadResult(
